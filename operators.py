525d22a79985aea052ee8ac141c3ec19f72bb9ee
mainamercy469@gmail.com <mainamercy469@gmail.com>
2026-02-02 16:34:50 +0300

    added 3 .py file

---
+++ operators.py
@@ -0,0 +1,16 @@
+print(5+4)
+print(5-4)
+print(5*4)
+print(5/4)
+print(5//4)
+print(5%4)
+print(5**4)
+a=50
+b=3
+
+print("the value of", a ,"+",3,"is:", a+b)
+a=input("Enter your name:")
+print("My name is",a)
+x=input("enter first number:")
+y=input("enter second number:")
+print(x=y)
